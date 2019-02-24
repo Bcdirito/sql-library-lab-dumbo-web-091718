828e4724a7520c46db30ae70ff83bc4a5657af67
Brian Di Rito <bdirito91@gmail.com>
2019-02-24 23:11:47 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,30 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
-  author TEXT,
-  sub_genre TEXT
+  author_id INTEGER,
+  sub_genre_id INTEGER
+);
+
+CREATE TABLE sub_genre (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
 );
