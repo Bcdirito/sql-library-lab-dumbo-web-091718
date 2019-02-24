6f08c73fc30e9587572c8e64df58fb036e487640
Brian Di Rito <bdirito91@gmail.com>
2019-02-24 23:28:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,34 +39,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT name, COUNT(characters.book_id)"
-end
-
-
-
-def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto
+  "SELECT name, COUNT(character_books)
   FROM characters
-  ORDER BY LENGTH(motto) DESC
-  LIMIT 1;"
-end
-
-
-
-def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto
-  FROM characters
-  ORDER BY LENGTH(motto) DESC
-  LIMIT 1;"
-end
-
-
-def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species)
-  FROM characters
-  GROUP BY species
-  ORDER BY COUNT(species) DESC
-  LIMIT 1;"
+  JOIN character_books
+  WHERE characters.id == character_books.chara"
 end
 
 
